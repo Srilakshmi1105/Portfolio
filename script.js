c08f861966928bce6a0eb65113beccc8df563e04
Srilakshmi1105 <152142357+Srilakshmi1105@users.noreply.github.com>
2025-08-07 16:11:06 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById("toggle-theme");
+
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  toggleButton.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+});
